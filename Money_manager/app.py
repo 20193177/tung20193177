32970acc5c10f4859b08cbfed1f3cb0471435c24
do thanh tung <you@example.com>
2022-07-22 21:34:14 +0700

    first commit

---
+++ Money_manager/app.py
@@ -0,0 +1,224 @@
+import db
+from tkinter import *
+from tkinter.ttk import *
+from functools import partial
+
+
+LARGE_FONT = ("Verdana", 32)
+
+class MoneyManager:
+    def __init__(self, master):
+        self.frame = Frame(master)
+        self.frame.pack()
+        self.main_window()
+
+    ### hiển thị các hàm để cập nhật cơ sở dữ liệu và thêm vào hoặc xóa#
+    def added(self, boxaile):
+        myLabel = Label(boxaile, text="Giao dịch đã được thêm")
+        myLabel.grid(row=4, column=0)
+
+    def delete(self, boxaile):
+        myLabel = Label(boxaile, text="Giao dịch đã bị xóa")
+        myLabel.grid(row=4, column=0)
+
+    def display_all(self, database):
+        select_all = database
+        return select_all
+
+    def insert(self, database, val1, val2, val3):
+        goods = val1.get()
+        price = val2.get()
+        date = val3.get()
+        insertion = database(goods, price, date)
+        return insertion
+
+    def find_expense(self, database, val1, val2):
+        goods = val1.get()
+        price = val2.get()
+        find = database(goods, price)
+        return find
+
+    def delete_expense(self,database, val1, val2):
+        goods = val1.get()
+        price = val2.get()
+        delete = database(goods, price)
+        return delete
+
+    ###Cửa sổ chính###
+    def main_window(self):
+        button1 = Button(self.frame, text="Khoản thu nhập", command=self.incomes)
+        button1.pack()
+
+        button2 = Button(self.frame, text="Khoản chi tiêu", command=self.expenses)
+        button2.pack()
+
+        button3 = Button(self.frame, text="Khoản vay",command=self.loan)
+        button3.pack()
+
+        button4 = Button(self.frame, text="Giao dịch khác", command=self.other)
+        button4.pack()
+
+        button5 = Button(self.frame, text="EXIT", command=exit)
+        button5.pack()
+
+    ###thêm giao dịch###
+    def incomes(self):
+
+
+        top = Toplevel(self.frame)
+        top.title('Khoản thu nhập')
+        l1 = Label(top, text="Tên loại thu nhập").grid(row = 1, column = 0, sticky = W, pady = 2)
+        l2 = Label(top, text="Số tiền").grid(row = 2, column = 0, sticky = W, pady = 2)
+        l3 = Label(top, text="Ngày giao dịch").grid(row = 3, column = 0, sticky = W, pady = 2)
+
+        e1 = Entry(top)
+        e1.grid(row=1, column=1, sticky=W, pady=2)
+        e2 = Entry(top)
+        e2.grid(row=2, column=1, sticky=W, pady=2)
+        e3 = Entry(top)
+        e3.grid(row=3, column=1, sticky=W, pady=2)
+
+        text = Text(top, width=40, height=10)
+        text.grid(row=5, column=1, columnspan=2)
+
+        #BUTTONS###
+
+        B1 = Button(top, text="Nhập giao dịch", command=lambda: (self.insert(db.insert_incomes,e1,e2,e3), self.added(top)))
+        B1.grid(row=1, column=2)
+
+        B2 = Button(top, text="Hiển thị giao dịch", command=lambda: (text.delete(1.0, END), text.insert(END, self.display_all(db.select_all_incomes()))))
+        B2.grid(row=2, column=2)
+
+        B3 = Button(top, text="Tìm giao dịch", command=lambda: (text.delete(1.0, END), text.insert(END, self.find_expense(db.select_incomes, e1,e2))))
+        B3.grid(row=2, column=3)
+
+        B3 = Button(top, text="Xóa giao dịch", command=lambda: (self.delete_expense(db.delete_incomes, e1,e2), self.delete(top)))
+        B3.grid(row=4, column=2)
+
+        B5= Button(top, text="Exit", command=exit)
+        B5.grid(row=4, column=3)
+
+
+    def expenses(self):
+        top = Toplevel(self.frame)
+        top.title('Khoản chi tiêu')
+        l1 = Label(top, text="Tên loại chi tiêu").grid(row=1, column=0, sticky=W, pady=2)
+        l2 = Label(top, text="Số tiền").grid(row=2, column=0, sticky=W, pady=2)
+        l3 = Label(top, text="Ngày giao dịch").grid(row=3, column=0, sticky=W, pady=2)
+
+        e1 = Entry(top)
+        e1.grid(row=1, column=1, sticky=W, pady=2)
+        e2 = Entry(top)
+        e2.grid(row=2, column=1, sticky=W, pady=2)
+        e3 = Entry(top)
+        e3.grid(row=3, column=1, sticky=W, pady=2)
+
+        text = Text(top, width=40, height=10)
+        text.grid(row=5, column=1, columnspan=2)
+
+        # BUTTONS###
+
+        B1 = Button(top, text="Nhập giao dịch",
+                    command=lambda: (self.insert(db.insert_expenses, e1, e2, e3), self.added(top)))
+        B1.grid(row=1, column=2)
+
+        B2 = Button(top, text="Hiển thị giao dịch", command=lambda: (text.delete(1.0, END), text.insert(END, self.display_all(db.select_all_expenses()))))
+        B2.grid(row=2, column=2)
+
+        B3 = Button(top, text="Tìm giao dịch", command=lambda: (
+        text.delete(1.0, END), text.insert(END, self.find_expense(db.select_expenses, e1, e2))))
+        B3.grid(row=2, column=3)
+
+        B3 = Button(top, text="Xóa giao dịch",
+                    command=lambda: (self.delete_expense(db.delete_expenses, e1, e2), self.delete(top)))
+        B3.grid(row=4, column=2)
+
+        B5 = Button(top, text="Exit", command=exit)
+        B5.grid(row=4, column=3)
+
+    def loan(self):
+        top = Toplevel(self.frame)
+        top.title('Khoản vay')
+        l1 = Label(top, text="Tên loại Khoản vay").grid(row=1, column=0, sticky=W, pady=2)
+        l2 = Label(top, text="Giá tiền").grid(row=2, column=0, sticky=W, pady=2)
+        l3 = Label(top, text="Ngày giao dịch").grid(row=3, column=0, sticky=W, pady=2)
+
+        e1 = Entry(top)
+        e1.grid(row=1, column=1, sticky=W, pady=2)
+        e2 = Entry(top)
+        e2.grid(row=2, column=1, sticky=W, pady=2)
+        e3 = Entry(top)
+        e3.grid(row=3, column=1, sticky=W, pady=2)
+
+        text = Text(top, width=40, height=10)
+        text.grid(row=5, column=1, columnspan=2)
+
+        # BUTTONS###
+
+        B1 = Button(top, text="Nhập giao dịch",
+                    command=lambda: (self.insert(db.insert_loan, e1, e2, e3), self.added(top)))
+        B1.grid(row=1, column=2)
+
+        B2 = Button(top, text="Hiển thị giao dịch", command=lambda: (
+        text.delete(1.0, END), text.insert(END, self.display_all(db.select_all_loan()))))
+        B2.grid(row=2, column=2)
+
+        B3 = Button(top, text="Tìm giao dịch", command=lambda: (
+        text.delete(1.0, END), text.insert(END, self.find_expense(db.select_loan, e1, e2))))
+        B3.grid(row=2, column=3)
+
+        B3 = Button(top, text="Xóa giao dịch",
+                    command=lambda: (self.delete_expense(db.delete_loan, e1, e2), self.delete(top)))
+        B3.grid(row=4, column=2)
+
+        B5 = Button(top, text="Exit", command=exit)
+        B5.grid(row=4, column=3)
+
+    def other(self):
+        top = Toplevel(self.frame)
+        top.title('Giao dịch khác')
+        l1 = Label(top, text="Tên khoản giao dịch").grid(row=1, column=0, sticky=W, pady=2)
+        l2 = Label(top, text="Giá tiền").grid(row=2, column=0, sticky=W, pady=2)
+        l3 = Label(top, text="Ngày giao dịch").grid(row=3, column=0, sticky=W, pady=2)
+
+        e1 = Entry(top)
+        e1.grid(row=1, column=1, sticky=W, pady=2)
+        e2 = Entry(top)
+        e2.grid(row=2, column=1, sticky=W, pady=2)
+        e3 = Entry(top)
+        e3.grid(row=3, column=1, sticky=W, pady=2)
+
+        text = Text(top, width=40, height=10)
+        text.grid(row=5, column=1, columnspan=2)
+
+        # BUTTONS###
+
+        B1 = Button(top, text="Nhập giao dịch",
+                    command=lambda: (self.insert(db.insert_other, e1, e2, e3), self.added(top)))
+        B1.grid(row=1, column=2)
+
+        B2 = Button(top, text="Hiển thị giao dịch", command=lambda: (
+            text.delete(1.0, END), text.insert(END, self.display_all(db.select_all_other()))))
+        B2.grid(row=2, column=2)
+
+        B3 = Button(top, text="Tìm giao dịch", command=lambda: (
+            text.delete(1.0, END), text.insert(END, self.find_expense(db.select_other, e1, e2))))
+        B3.grid(row=2, column=3)
+
+        B3 = Button(top, text="Xóa giao dịch",
+                    command=lambda: (self.delete_expense(db.delete_other, e1, e2), self.delete(top)))
+        B3.grid(row=4, column=2)
+
+        B5 = Button(top, text="Exit", command=exit)
+        B5.grid(row=4, column=3)
+
+
+def main():
+    root = Tk()
+    root.geometry('250x200')
+    root.title("Money Manager")
+    MoneyManager(root)
+    root.mainloop()
+
+
+main()
---
+++ Money_manager/db.py
@@ -0,0 +1,224 @@
+import sqlite3
+import datetime
+now = datetime.datetime.utcnow()
+
+CREATE_INCOMES = "CREATE TABLE IF NOT EXISTS incomes (id INTEGER PRIMARY KEY,good TEXT, price INTEGER, date DATE);"
+CREATE_EXPENSES = "CREATE TABLE IF NOT EXISTS expenses (id INTEGER PRIMARY KEY,good TEXT, price INTEGER, date DATE);"
+CREATE_LOAN = "CREATE TABLE IF NOT EXISTS loan (id INTEGER PRIMARY KEY,good TEXT, price INTEGER, date DATE);"
+CREATE_OTHER = "CREATE TABLE IF NOT EXISTS other (id INTEGER PRIMARY KEY,good TEXT, price INTEGER, date DATE);"
+
+
+
+INSERT_INCOMES = "INSERT INTO incomes (good, price, date) VALUES(?,?,?);"
+INSERT_EXPENSES = "INSERT INTO expenses (good, price, date) VALUES(?,?,?);"
+INSERT_LOAN = "INSERT INTO loan (good, price, date) VALUES(?,?,?);"
+INSERT_OTHER = "INSERT INTO other (good, price, date) VALUES(?,?,?);"
+
+
+SELECT_ALL1 = "SELECT * FROM incomes;"
+SELECT_ALL2 = "SELECT * FROM expenses;"
+SELECT_ALL3 = "SELECT * FROM loan;"
+SELECT_ALL4 = "SELECT * FROM other;"
+
+SELECT_INCOMES = "SELECT * FROM incomes WHERE good = ? AND price = ?;"
+SELECT_EXPENSES = "SELECT * FROM expenses WHERE good = ? AND price = ?;"
+SELECT_LOAN = "SELECT * FROM loan WHERE good = ? AND price = ?;"
+SELECT_OTHER = "SELECT * FROM other WHERE good = ? AND price = ?;"
+
+DELETE_INCOMES = "DELETE FROM incomes WHERE good = ? AND price = ?;"
+DELETE_EXPENSES = "DELETE FROM expenses WHERE good = ? AND price = ?;"
+DELETE_LOAN = "DELETE FROM loan WHERE good = ? AND price = ?;"
+DELETE_OTHER = "DELETE FROM other WHERE good = ? AND price = ?;"
+
+
+
+
+
+###create for every table###
+def create_tables():
+    conn = sqlite3.connect('data.db')
+    with conn:
+        return conn.execute(CREATE_OTHER)
+
+###INSERT VALUES###
+
+def insert_incomes(good, price, date):
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(INSERT_INCOMES, (good, price, date))
+        conn.commit()
+
+
+
+def insert_expenses(good, price, date):
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(INSERT_EXPENSES , (good, price, date))
+        conn.commit()
+        c.close()
+
+def insert_loan(good, price, date):
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(INSERT_LOAN, (good, price, date))
+        conn.commit()
+        c.close()
+
+def insert_other(good, price, date):
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(INSERT_OTHER, (good, price, date))
+        conn.commit()
+        c.close()
+
+###SELECT_ALL###
+
+
+
+def select_all_incomes():
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(SELECT_ALL1)
+        #have to store data into a list of Tuple
+        list = c.fetchall()
+        c.close()
+        output = ''
+        for x in list:
+            output = output + str(x[1]) + ' ' + str(x[2]) + ' ' + ' ' + str(x[3]) + '\n'
+        return output
+
+
+
+
+
+def select_all_expenses():
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(SELECT_ALL2)
+        # have to store data into a list of Tuple
+        list = c.fetchall()
+        c.close()
+        output = ''
+        for x in list:
+            output = output + str(x[1]) + ' ' + str(x[2]) + ' ' + ' ' + str(x[3]) + '\n'
+        return output
+
+def select_all_loan():
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(SELECT_ALL3)
+        # have to store data into a list of Tuple
+        list = c.fetchall()
+        c.close()
+        output = ''
+        for x in list:
+            output = output + str(x[1]) + ' ' + str(x[2]) + ' ' + ' ' + str(x[3]) + '\n'
+        return output
+
+def select_all_other():
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(SELECT_ALL4)
+        # have to store data into a list of Tuple
+        list = c.fetchall()
+        c.close()
+        output = ''
+        for x in list:
+            output = output + str(x[1]) + ' ' + str(x[2]) + ' ' + ' ' + str(x[3]) + '\n'
+        return output
+
+###SELECT SPECIFIC###
+
+def select_incomes(good, price):
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(SELECT_INCOMES, (good, price))
+        # have to store data into a list of Tuple
+        list = c.fetchall()
+        c.close()
+        output = ''
+        for x in list:
+            output = output + str(x[1]) + ' ' + str(x[2]) + ' ' + ' ' + str(x[3]) + '\n'
+        return output
+
+def select_expenses(good, price):
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(SELECT_EXPENSES, (good, price))
+        # have to store data into a list of Tuple
+        list = c.fetchall()
+        c.close()
+        output = ''
+        for x in list:
+            output = output + str(x[1]) + ' ' + str(x[2]) + ' ' + ' ' + str(x[3]) + '\n'
+        return output
+
+def select_loan(good, price):
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(SELECT_LOAN, (good, price))
+        # have to store data into a list of Tuple
+        list = c.fetchall()
+        c.close()
+        output = ''
+        for x in list:
+            output = output + str(x[1]) + ' ' + str(x[2]) + ' ' + ' ' + str(x[3]) + '\n'
+        return output
+
+def select_other(good, price):
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(SELECT_OTHER, (good, price))
+        # have to store data into a list of Tuple
+        list = c.fetchall()
+        c.close()
+        output = ''
+        for x in list:
+            output = output + str(x[1]) + ' ' + str(x[2]) + ' ' + ' ' + str(x[3]) + '\n'
+        return output
+
+
+###DELETE VALUE###
+def delete_incomes(good, price):
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(DELETE_INCOMES, (good, price))
+        conn.commit()
+        c.close()
+
+def delete_expenses(good, price):
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(DELETE_EXPENSES, (good, price))
+        conn.commit()
+        c.close()
+
+def delete_loan(good, price):
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(DELETE_LOAN, (good, price))
+        conn.commit()
+        c.close()
+
+def delete_other(good, price):
+    conn = sqlite3.connect('data.db')
+    with conn:
+        c = conn.cursor()
+        c.execute(DELETE_OTHER, (good, price))
+        conn.commit()
+        c.close()
